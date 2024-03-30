c9bdf1bd5a5300523c5495290e8e7f1932671e38
Akshat Nehra <akshatnehra1999@gmail.com>
2024-03-29 23:06:06 -0400

    feat: Update for website compatibility changes

--- README.md
+++ README.md
@@ -9,7 +9,9 @@ This script downloads OSHO's discourses from [OSHO World](https://oshoworld.com/
 ## Usage
 
 - You must have python-3 and pip3 installed on your system
-- Clone the repository or [download](https://github.com/SurjitSahoo/oshoworld-discourse-downloader/archive/refs/heads/main.zip) the package and install the dependencies with following command  
-  `pip install -r requirements.txt`
-- Run the script to start downloading with following command  
-  `python scrape.py`
+- Clone the repository or [download](https://github.com/SurjitSahoo/oshoworld-discourse-downloader/archive/refs/heads/main.zip) the package and install the dependencies with following command`pip install -r requirements.txt`
+- Run the script to start downloading with following command`python scrape.py`
+
+## Credits
+
+This project builds upon the original work of **Surjit Sahoo's** OSHO Discourses downloader. With the website's recent updates, I've made necessary changes to ensure the tool's compatibility. Special thanks to Surjit for the foundational work. Original repository: [OSHO Discourses downloader](https://github.com/SurjitSahoo/oshoworld-discourse-downloader).
--- scrape.py
+++ scrape.py
@@ -1,102 +1,95 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import os.path
-import json
 from pySmartDL import SmartDL
 import logging
 import sys
+import ssl
 
-# baseURL = 'https://oshoworld.com/audio-discourse-hindi-'
+# Disable SSL certificate verification
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Disable SSL certificate verification
+requests.packages.urllib3.disable_warnings()
+
 baseURL = 'https://oshoworld.com/osho-audio-discourse-english-'
+# baseURL = 'https://oshoworld.com/audio-discourse-hindi-'
 
 download_dir = 'downloads/English'
-cache_dir = 'cache'
+
+if baseURL.endswith('hindi-'):
+    download_dir = 'downloads/Hindi'
+
+# Select Hindi directory if baseurl is Hindi
 
 if not os.path.exists(download_dir):
-	os.makedirs(download_dir)
-
-if not os.path.exists(cache_dir):
-	os.makedirs(cache_dir)
+    os.makedirs(download_dir)
 
 headers = {
-	 "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
+     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0"
 }
 
+# alphabets = ['g']
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stdout))
 
-def cached_get(url: str, is_json=False):
-	file_name = url.split('/')[-2] if url.endswith('/') else url.split('/')[-1]
-	if file_name.startswith('?'):
-		file_name = file_name[1:]
-	file_path = os.path.join(os.getcwd(), cache_dir, file_name + '.json' if is_json else file_name + '.html')
-
-	if os.path.isfile(path=file_path):
-		with open(file_path, 'rt', encoding='utf-8') as f:
-			if is_json:
-				return json.load(f)
-			else:
-				return f.read()
-	else:
-		req = requests.get(url, headers=headers)
-		content = req.json() if is_json else requests.get(url, headers=headers).text
-		with open(file_path, mode='wt', encoding='utf-8') as f:
-			if is_json:
-				f.write(json.dumps(content))
-			else:
-				f.write(content)
-		return content
-
 def download_discourse(url: str, base_dir: str):
-	logger.info('Finding tracks for Discourse ' + url + '...')
-	try:
-		soup = BeautifulSoup(cached_get(url), 'html.parser')
-		discourse = soup.find('meta', attrs={'property': 'og:title'})['content'].split(' # ')[0]
-		discourse_dir = base_dir + '/' + discourse
-		
-		if not os.path.exists(discourse_dir):
-			os.makedirs(discourse_dir)
-
-		audio_igniter = soup.find('div', attrs={'class': 'audioigniter-root'})['data-tracks-url']
-		tracks = cached_get(url=audio_igniter, is_json=True)
-		for track in tracks:
-			file_name = track['downloadFilename'] or track['title'] + '.mp3'
-			download_url = track['downloadUrl'] or track['audio']
-			if os.path.isfile(path=discourse_dir + '/' + file_name):
-				logger.info('Track ' + track['title'] + ' already exists. Skipping...')
-				continue
-
-			logger.info('>> Downloading ' + file_name + ' : ' + download_url)
-			dl = SmartDL(download_url, dest=discourse_dir + '/' + file_name, timeout=600)
-			try:
-				dl.start()
-				if dl.isFinished() and dl.isSuccessful():
-					logger.info('>> Downloaded ' + file_name)
-				else:
-					logger.error('>> Failed to download ' + file_name)
-			except Exception as e:
-				logger.exception(e)
-				continue
-	except Exception as e:
-		logger.exception(e)
-	
+    logger.info('Finding tracks for Discourse ' + url + '...')
+    try:
+        soup = BeautifulSoup(requests.get(url, headers=headers, verify=False).text, 'html.parser')
+        discourse = soup.find('meta', attrs={'property': 'og:title'})['content'].split(' # ')[0]
+        discourse_dir = os.path.join(base_dir, discourse)
+        
+        if not os.path.exists(discourse_dir):
+            os.makedirs(discourse_dir)
+        
+        table = soup.find('figure', class_='wp-block-table')
+        rows = table.find_all('tr')
+        
+        for row in rows:
+            cells = row.find_all('td')
+            if len(cells) == 2:
+                url = cells[1].find('a')['href']
+                soup = BeautifulSoup(requests.get(url, headers=headers, verify=False).text, 'html.parser')
+                title = cells[1].text.strip()
+                audio_tag = soup.find('div', class_="audio_frame audio_local")
+                download_link = audio_tag.find('source')['src']
+                file_name = title + '.mp3'  # Extracting the discourse number from the title
+                file_path = os.path.join(discourse_dir, file_name)
+                
+                if os.path.isfile(file_path):
+                    logger.info('Track ' + title + ' already exists. Skipping...')
+                    continue
+                
+                logger.info('>> Downloading ' + file_name + ' : ' + download_link)
+                dl = SmartDL(download_link, dest=file_path, timeout=600)
+                try:
+                    dl.start()
+                    if dl.isFinished() and dl.isSuccessful():
+                        logger.info('>> Downloaded ' + file_name)
+                    else:
+                        logger.error('>> Failed to download ' + file_name)
+                except Exception as e:
+                    logger.exception(e)
+    except Exception as e:
+        logger.exception(e)
 
 for alphabet in alphabets:
-	dir_alpha = download_dir + '/' + alphabet
+    dir_alpha = os.path.join(download_dir, alphabet)
 
-	if not os.path.exists(dir_alpha):
-		os.makedirs(dir_alpha)
+    if not os.path.exists(dir_alpha):
+        os.makedirs(dir_alpha)
 
-	logger.info('Finding discourses with ' + alphabet.capitalize() + '...')
-	url = baseURL + alphabet + '/'
-	soup = BeautifulSoup(cached_get(url), 'html.parser')
-	discourses = soup.find_all('a', string="Play & Download", href=True)
-	discourse_links = [discourse['href'] for discourse in discourses]
+    logger.info('Finding discourses with ' + alphabet.capitalize() + '...')
+    url = baseURL + alphabet + '/'
+    soup = BeautifulSoup(requests.get(url, headers=headers, verify=False).text, 'html.parser')
+    discourses = soup.find_all('a', string="Play & Download", href=True)
+    discourse_links = [discourse['href'] for discourse in discourses]
 
-	for discourse in discourse_links:
-		download_discourse(discourse, dir_alpha)
+    for discourse in discourse_links:
+        download_discourse(discourse, dir_alpha)
 
 logger.info('>>>> Completed all downloads! <<<<')
